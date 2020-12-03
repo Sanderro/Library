ce8f619ef01ee04fc434d9f3a732424c368f3818
Sanderro <kifonidis.a@gmail.com>
2020-12-03 20:41:44 +0100

    Changed book and borrows models

--- db/migrate/20201203192542_create_borrows.rb
+++ db/migrate/20201203192542_create_borrows.rb
@@ -1,8 +1,11 @@
 class CreateBorrows < ActiveRecord::Migration[6.0]
   def change
     create_table :borrows do |t|
-      t.string :odkiedy
-      t.string :dokiedy
+      t.integer :borrowid
+      t.integer :memberid
+      t.integer :bookid
+      t.date :datefrom
+      t.date :dateto
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_183131) do
+ActiveRecord::Schema.define(version: 2020_12_03_192542) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,16 +21,11 @@ ActiveRecord::Schema.define(version: 2020_11_17_183131) do
   end
 
   create_table "borrows", force: :cascade do |t|
-    t.string "odkiedy"
-    t.string "dokiedy"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "members", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "email"
+    t.integer "borrowid"
+    t.integer "memberid"
+    t.integer "bookid"
+    t.date "datefrom"
+    t.date "dateto"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
