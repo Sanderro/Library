4119a3dc917b62ac2fa836be86b94832ce32e335
Sanderro <kifonidis.a@gmail.com>
2020-12-06 01:30:41 +0100

    Added Edit and Delete actions to BooksController; Added Edit form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_index_path
   end
 
   private
