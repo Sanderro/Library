0ef05926e2203bc04dc5649652d7adf834b513ee
Sanderro <kifonidis.a@gmail.com>
2021-01-24 12:59:49 +0100

    Changed email format validation regex to Devise validation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,7 @@ class MembersController < ApplicationController
   validates :firstname, length: { maximum: 20}
   validates :lastname, length: { maximum: 20}
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
+  validates :email, format: Devise.email_regexp
 
   before_action :authenticate_member!
   def index
