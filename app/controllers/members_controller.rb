b022e00de581af174bd3e575751e35834c9917e2
Sanderro <kifonidis.a@gmail.com>
2021-01-24 12:56:58 +0100

    Added Members model validations for firstname,lastname,email

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,9 @@
 class MembersController < ApplicationController
+  validates :firstname, length: { maximum: 20}
+  validates :lastname, length: { maximum: 20}
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
+
   before_action :authenticate_member!
   def index
     @members = Member.all
